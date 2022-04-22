8823a91379ba05572afc93f80040c2b7307a78e8
Matthias GOUPIL <matgo@P-MPL-FROMENTAL.dhcp.mpl.intranet>
2022-04-22 11:30:22 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../assets/logo.svg';
+import '../styles/App.css'; 
+import Header from "./Header"
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import "../styles/Header.css"
+
+function Header(){
+    return <header>
+        <div className="logo">
+            <h1>Matthias Goupil</h1>
+            <p>Etudiant en informatique</p>
+        </div>
+    </header>
+}
+
+export default Header
