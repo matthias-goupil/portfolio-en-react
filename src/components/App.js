32daa84a4918d00495cb7428e97cda57b732d483
Matthias GOUPIL <matgo@P-MPL-FROMENTAL.dhcp.mpl.intranet>
2022-04-28 16:21:52 +0200

    plus qu'à corriger certains bugs au niveau du côté responsive et à ajouter quelques images et des liens pour les pojets

--- src/components/App.js
+++ src/components/App.js
@@ -45,8 +45,17 @@ import bangProject from "../assets/projects/Bang.png"
 import eCommerceProject from "../assets/projects/e-commerce.png"
 import flappyBirdProject from "../assets/projects/FlappyBird.png"
 import instagram from "../assets/projects/Instagram.png"
+import instagram1 from "../assets/projects/Instagram/1.png"
+import instagram2 from "../assets/projects/Instagram/2.png"
+import instagram3 from "../assets/projects/Instagram/3.png"
+import instagram4 from "../assets/projects/Instagram/4.png"
+import instagram5 from "../assets/projects/Instagram/5.png"
+import instagram6 from "../assets/projects/Instagram/6.png"
+import instagram7 from "../assets/projects/Instagram/7.png"
+import instagram8 from "../assets/projects/Instagram/8.png"
 
 import photoProfil from "../assets/photoProfil.jpg"
+
 
 
 function App() {
@@ -58,11 +67,11 @@ function App() {
           <div>
             <h2 className='title'>à propos de moi</h2>
             <p>
-Je m'appelle Matthias, j'ai 19 ans et suis en deuxième année de DUT informatique à l'IUT de Montpellier. Le développement web est actuellement le domaine qui me plait le plus dans l'informatique et je veux en faire mon métier. Je suis d'ailleurs à la recherche d'un stage dans ce domaine là. Quand j'ai du temps libre, j'aime découvrir et apprendre à utiliser de nouvelles technologies. 
+Je m'appelle Matthias, j'ai 19 ans et suis en deuxième année de DUT informatique à l'IUT de Montpellier. Le développement web est actuellement le domaine qui me plait le plus dans l'informatique et je veux en faire mon métier. En ce moment, je suis stagiaire développeur web chez SMILE. Quand j'ai du temps libre, l'une des mes occupations est de découvrir et d'apprendre à utiliser de nouvelles technologies. 
             </p>
             <a href="#"> Mon CV<span></span><span></span><span></span><span></span></a>
           </div>
-          <img alt="" src={photoProfil}></img>
+          <img target="_blank" alt="" src={photoProfil}></img>
         </section>
       <WaveSection title="Mes expériences" id="experiences">
         {experiences.map((experience) => {
@@ -81,7 +90,7 @@ Je m'appelle Matthias, j'ai 19 ans et suis en deuxième année de DUT informatiq
       <Section id="projects" title="Projets réalisés">
         <div>
           {projects.map((project) => {
-            return <Project nom={project.name} image={project.image} github={project.github}>
+            return <Project images={project.images} nom={project.name} image={project.image} github={project.github}>
             {project.content}
             </Project>
           })}
@@ -218,38 +227,77 @@ const projects = [
   {
     name: "Instagram en Symfony",
     image: instagram,
-    content: <p>Test</p>,
-    github: "https://github.com/matthias-goupil/instagram-symfony"
+    content: <React.Fragment>
+      <div>
+        <p>Afin de m'entrainer à l'utilisation du framework Symfony, je me suis mis pour objectif de réaliser un réseau-social. <br />Pour cela, je me suis inspiré de Instagram, un réseau social permettant de partager des photos.</p>
+        <p className='Pliste'>Fonctionnalités implémentées :</p>
+        <ul>
+          <li>Authentification / inscription d'un utilisateur</li>
+          <li>Gestion d'un profil utilisateur</li>
+          <li>Option de modification / suprression d'un profil utilisateur</li>
+          <li>option d'abonnement à un utilisateur</li>
+          <li>Système d'ajout de publication pour partager une photo et la commenter</li>
+          <li>Option de like pour aimer une publication</li>
+        </ul>
+
+        <p className='Pliste'>Principales technologies utilisées</p>
+        <ul>
+          <li>le framework Symfony</li>
+          <li>l'ORM Doctrine</li>
+          <li>Le moteur de template Twig</li>
+        </ul>
+      </div>
+      
+    </React.Fragment>,
+    github: "https://github.com/matthias-goupil/instagram-symfony",
+    images: [instagram1,instagram2,instagram3,instagram5,instagram6,instagram7,instagram4,instagram8]
   },
   {
     name: "site portfolio",
     image: portfolio,
-    content: <p>Test</p>,
-    github: "https://github.com/matthias-goupil/portfolio-en-react"
+    content: <div>
+      <p>Réalisation de mon site portfolio avec la bibiliothèque Javascript Reactdans le but de m'entrainer.<br />Ce site sert principalement à me contacter et présenter mes expériences, mes compétences et mes projets.<br />Vous êtes actuellement en train de l'utiliser.</p>
+    </div>,
+    github: "https://github.com/matthias-goupil/portfolio-en-react",
+    images: []
+
   },
   {
     name: "Application web pour la Serre de l'IUT de Montpellier",
     image: serreProject,
-    content: <p>Test</p>,
-    github: null
+    content: <p>Pour le projet tuteuré du 3 semestre de mon DUT, avec mon groupe, nous avons réalisé une application web pour la serre connectée de l’IUT.<br />
+    En effet, le bâtiment A de l’IUT abrite une serre connectée et une grande grande partie du fonctionnement de cette serre est automatisée (Filtration, arrosage des plantes, etc.. ). Néanmoins une supervision humaine était requise pour un certain nombre de contrôles et d’actions.
+    Exemple: Le niveau de l’eau est-il assez haut ? Si non faire un complément ?<br />
+    Seul un tout petit nombre de personnes réalisaient ces tâches. Et par manque d’outil de suivi, plusieurs personnes pouvaient passer chaque jour pour contrôler (et se déplaçaient pour rien si quelqu’un était déjà passé).
+    <br />L’idée de notre application web est donc de gérer ces interventions de contrôle et ce de façon planifiée.</p>,
+    github: null,
+    images: [instagram1]
+
   },
   {
     name: "Site e-commerce en PHP",
     image: eCommerceProject,
-    content: <p>Test</p>,
-    github: "https://github.com/matthias-goupil/projet-e-Commerce-IUT"
+    content: <p>Réalisé dans l'un des cours de mon DUT, ce projet de groupe avait pour but de concevoir un e-commerce (sans la partie paiement) afin de mieux se familiariser au langage PHP et au design pattern MVC.
+    <br />Le côté responsive du site et la partie paiement n'a donc pas été implémentée. </p>,
+    github: "https://github.com/matthias-goupil/projet-e-Commerce-IUT",
+    images: [instagram1]
+
   },
   {
     name: "IHM du jeu \"Bang !\" en JavaFx",
     image: bangProject,
     content: <p>Test</p>,
-    github: null
+    github: null,
+    images: [instagram1]
+
   },
   {
     name: "Flappy Bird en Javascript",
     image: flappyBirdProject,
-    content: <p>Test</p>,
-    github: null
+    content: <p>Ce projet consiste à la réalisation d'un Flappy Bird grâce à la balise canvas, qui permet de dessiner des formes en Javascript. <br />J'avais pour but, en réalisant celui-ci de me familiariser avec le Javascript que j'étais en train d'apprendre mais aussi, je voulais apprendre à réaliser des jeux en 2D.</p>,
+    github: null,
+    images: [instagram1]
+
   }
 ]
 
--- src/components/Project.js
+++ src/components/Project.js
@@ -11,14 +11,27 @@ function Project(props){
                 <a href="#" onClick={(e) => {
                     e.preventDefault();
                     setMoreDetails(true)
+                    document.body.style.overflowY = "hidden";
                     }}>En savoir plus</a>
             </div>
         </div>
         <div className={moreDetails?"fenetreModale open":"fenetreModale"}>
                     <div className="content">
-                        <button onClick={() => setMoreDetails(false)}>Fermer</button>
+                        <button onClick={() => {
+                            setMoreDetails(false)
+                    document.body.style.overflowY = "auto";
+                            
+                            }}>Fermer</button>
                         <h2>{props.nom}</h2>
-                        {props.children}
+                        <div>
+                            {props.children}
+                            {props.images.length > 0 ?<React.Fragment>
+                                <h3>Le projet en image</h3>
+                            <div className="images">
+                                {props.images.map((image) => <img src={image} alt="" key={image}/>)}
+                            </div></React.Fragment>:null}
+                            
+                        </div>
                     </div>
                     <div className="buttonBar">
                         {props.github && <a target="_blank" href={props.github}>Github</a>}
